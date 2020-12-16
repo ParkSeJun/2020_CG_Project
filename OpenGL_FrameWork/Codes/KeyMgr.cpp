c8bda82509b8a8d4714de731e290881da8eda368
ParkSeJun <chunsa1166@naver.com>
2020-12-16 09:03:04 +0900

    r키 패치

--- OpenGL_FrameWork/Codes/KeyMgr.cpp
+++ OpenGL_FrameWork/Codes/KeyMgr.cpp
@@ -52,7 +52,7 @@ void CKeyMgr::Update()
 		m_dwKey |= KEY_2;
 	if (GetAsyncKeyState('3') & 0x8000)
 		m_dwKey |= KEY_3;
-	if (GetAsyncKeyState('r') & 0x8000)
+	if ((GetAsyncKeyState('r') & 0x8000) || (GetAsyncKeyState('R') & 0x8000))
 		m_dwKey |= KEY_R;
 }
 
